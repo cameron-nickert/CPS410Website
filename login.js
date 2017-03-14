aaedbb39d43c29e65d1a5b1a06aca850c87eb6fd
Bryan Caragay <carag1ba@cmich.edu>
2017-03-13 20:27:46 -0400

    Website

---
+++ login.js
@@ -0,0 +1,23 @@
+function logIn(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+        window.location.href = "dashboard.html";
+
+        // User is signed in.
+        } else {
+        // No user is signed in.
+        }
+    });
+    
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      // ...
+    });
+}
+
